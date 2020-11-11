0c4da411277894ea406d4ef3012950121e8884f8
308229387 <>
2020-11-11 08:43:33 +0800

    retrofit

---
+++ app/src/main/java/com/example/activity/RetrofitActivity.java
@@ -0,0 +1,17 @@
+package com.example.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class RetrofitActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.service_layout);
+
+        
+
+    }
+}
